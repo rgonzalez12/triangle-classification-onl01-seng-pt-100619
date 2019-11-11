cbbe66d98c8810539b9a8f4fae8f9ac194e65060
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-11 22:58:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,14 +7,11 @@ class Triangle
     @side3 = side3
   end
   
-  class TriangleError < StandardError
-  end
+  class TriangleError < StandardError; end
   
   def kind 
     if side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2
-    begin
      raise TriangleError
-    end 
     elsif side1 == side2 && side3 == side2 && side1 == side3
      :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
