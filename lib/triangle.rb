8103e637e5a0508b6be62aa40d6ba28ac23f1e4d
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-12 01:40:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,16 +10,14 @@ class Triangle
   class TriangleError < StandardError; end
   
   def kind 
-    #if side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2
-     #raise TriangleError
+    if side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2
+     raise TriangleError
     if side1 == side2 && side3 == side2 && side1 == side3
      :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
      :isosceles
-    elsif side1 + side2 == side3 || side2 + side3 == side1 || side3 + side1 == side2
-     :scalene
     else
-     raise TriangleError
+     :scalene
     end
   end
 end
